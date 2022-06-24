ed85ac9eac10ceb08e19f83fee2a51948c61b3dc
tom <twm@qq.com>
2022-06-24 12:59:12 +0800

    aaa

--- doc/build_x86.sh
+++ doc/build_x86.sh
@@ -3,3 +3,5 @@
 set -ex
 
 cargo build --release --target x86_64-unknown-linux-musl --bin sync_server
+
+scp ../target/x86_64-unknown-linux-musl/release/sync_server user@192.168.1.26:/home/user/bak/tom/fy_admin/sync_server/
